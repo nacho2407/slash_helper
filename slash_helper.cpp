92c0974ecae50635965c9ad79b15d0ebfe5e31cf
Jang Minjun <jmj0ok@gmail.com>
2024-08-16 22:33:43 +0900

    Initial commit

---
+++ slash_helper.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char* argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    if(argc != 2) {
+        std::cerr << "Usage: ./slash_helper.exe <argv[1]>" << std::endl;
+
+        return 1;
+    }
+
+    std::ifstream input_file(argv[1]);
+    if (!input_file.is_open()) {
+        std::cerr << "Error: Could not open file " << argv[1] << std::endl;
+
+        return 1;
+    }
+
+    std::ofstream output_file("slashed.out");
+    if (!output_file.is_open()) {
+        std::cerr << "Error: Could not create output file." << std::endl;
+
+        return 1;
+    }
+
+    std::string line;
+    while (std::getline(input_file, line)) {
+        int length = line.length();
+        for(int i = 0; i < line.length(); i++) {
+            if(line[i] == '\t')
+                length += 3;
+        }
+
+        int count = 0;
+        if (line.empty())
+            count = 20;
+        else if(length < 80)
+            count = 20 - length / 4;
+
+        for (int i = 0; i < count; i++)
+            line += '\t';
+
+        output_file << line << "\\\n";
+    }
+
+    input_file.close();
+    output_file.close();
+
+    std::cout << "Processing complete." << std::endl;
+
+    return 0;
+}
